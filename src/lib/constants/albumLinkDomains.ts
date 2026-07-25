f0800c4a1bd5aa4490518e56d30e27246901d6c5
ccxtwf <theweirdfandom@gmail.com>
2026-07-26 04:38:12 +0700

    Lint changes

--- src/lib/constants/albumLinkDomains.ts
+++ src/lib/constants/albumLinkDomains.ts
@@ -8,23 +8,23 @@ export const ALBUM_STREAMING_LINKS: AlbumStreamingLink[] = [
   {
     name: "Niconico Crossfade",
     paramKey: "nn-xfade",
-    regex: /^https?:\/\/www\.nicovideo\.jp\/watch\/(?<embedid>[^/\?]+)/,
+    regex: /^https?:\/\/www\.nicovideo\.jp\/watch\/(?<embedid>[^/?]+)/,
   },
   {
     name: "YouTube Crossfade",
     paramKey: "yt-xfade",
     regex:
-      /^https?:\/\/(?:(?:|www\.)youtube\.com\/(?:watch\?v=|shorts\/)|youtu\.be\/)(?<embedid>[^/\?]{11})/,
+      /^https?:\/\/(?:(?:|www\.)youtube\.com\/(?:watch\?v=|shorts\/)|youtu\.be\/)(?<embedid>[^/?]{11})/,
   },
   {
     name: "Spotify",
     paramKey: "sp-embed",
-    regex: /^https?:\/\/open\.spotify\.com\/album\/(?<embedid>[^/\?]+)/,
+    regex: /^https?:\/\/open\.spotify\.com\/album\/(?<embedid>[^/?]+)/,
   },
   {
     name: "YouTube Music Playlist",
     paramKey: "yt-playlist",
-    regex: /^https?:\/\/(?:music\.|www\.|)youtube\.com\/playlist\?list=(?<embedid>[^/\?]+)/,
+    regex: /^https?:\/\/(?:music\.|www\.|)youtube\.com\/playlist\?list=(?<embedid>[^/?]+)/,
   },
   {
     name: "Bandamp Embed ID",
