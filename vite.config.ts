d92151a1b64717315920b85c977cf4023b6c7354
ccxtwf <theweirdfandom@gmail.com>
2026-07-26 00:48:41 +0700

    Migrate to Vite+

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,28 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig, lazyPlugins } from "vite-plus";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
-})
+  staged: {
+    "*": "vp check --fix",
+  },
+
+  fmt: {
+    ignorePatterns: [],
+
+    semi: true,
+    singleQuote: false,
+    arrowParens: "always",
+    embeddedLanguageFormatting: "auto",
+  },
+
+  lint: {
+    ignorePatterns: [],
+
+    jsPlugins: [{ name: "vite-plus", specifier: "vite-plus/oxlint-plugin" }],
+    rules: { "vite-plus/prefer-vite-plus-imports": "error" },
+    options: { typeAware: true, typeCheck: true },
+  },
+
+  plugins: lazyPlugins(() => [svelte()]),
+});
