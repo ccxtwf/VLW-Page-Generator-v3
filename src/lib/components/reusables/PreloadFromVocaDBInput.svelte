0d9116f99c5ab7e23691c73bf1c7c0ca51cf97d2
ccxtwf <theweirdfandom@gmail.com>
2026-07-26 17:01:55 +0700

    FEAT: Reusables

---
+++ src/lib/components/reusables/PreloadFromVocaDBInput.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  interface PreloadFromVocaDBInputProps {
+    handleFetch: (url: string) => void
+    placeholder: string
+  }
+
+  let { handleFetch, placeholder }: PreloadFromVocaDBInputProps = $props();
+  let vdbUrl = $state("");
+</script>
+
+<div class="join">
+  <div>
+    <label class="input join-item">
+      <input type="text" placeholder="{placeholder}" bind:value={vdbUrl} />
+    </label>
+  </div>
+  <button type="button" class="btn btn-neutral join-item" onclick={() => handleFetch(vdbUrl)}>Preload</button>
+</div>
---
+++ src/lib/components/reusables/Tooltip.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import type { Snippet } from "svelte";
+
+  interface ToolTipProps {
+    required?: boolean;
+    position?: 'top' | 'bottom' | 'left' | 'right';
+    align?: 'start' | 'end' | 'center';
+    simpleTooltip?: string
+    children?: Snippet
+  }
+  
+  let { simpleTooltip, required = false, position = 'top', align = 'start', children }: ToolTipProps = $props();
+</script>
+
+<div class="tooltip tooltip-top md:tooltip-{position} md:tooltip-{align}" data-tip={simpleTooltip}>
+  {#if children}
+    <div class="tooltip-content">
+      {@render children()}
+    </div>
+	{/if}
+  <div class="flex items-center justify-center w-4 h-4 rounded-full border-1 border-gray-400 text-gray-500 font-bold text-sm select-none hover:bg-gray-100 transition-colors cursor-pointer">
+    {#if required}
+      <div class="text-red-500">*</div>
+    {/if}
+    ?
+  </div>
+</div>
