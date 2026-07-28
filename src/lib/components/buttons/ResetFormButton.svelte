b0b66023d063f94dc05cf2f5410029f2d044a592
ccxtwf <theweirdfandom@gmail.com>
2026-07-28 21:05:06 +0700

    FIX: Reset form button

--- src/lib/components/buttons/ResetFormButton.svelte
+++ src/lib/components/buttons/ResetFormButton.svelte
@@ -1,10 +1,18 @@
 <script lang="ts">
   import { _ } from "svelte-i18n";
+
+  function handleClick(e: Event) {
+    if (!window.confirm($_("confirmClear"))) {
+      e.preventDefault();
+      return;
+    }
+  }
 </script>
 
 <button
   class="btn btn-error max-sm:w-full"
-  type="button"
+  type="reset"
+  onclick={handleClick}
 >
   {$_("formActions.reset")}
 </button>
