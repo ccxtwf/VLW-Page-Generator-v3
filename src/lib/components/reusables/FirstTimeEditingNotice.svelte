40fb578a2c3de879ec7f2782902e637a075a2af1
ccxtwf <theweirdfandom@gmail.com>
2026-08-09 21:47:38 +0700

    FIX: First time editing notice: styling

--- src/lib/components/reusables/FirstTimeEditingNotice.svelte
+++ src/lib/components/reusables/FirstTimeEditingNotice.svelte
@@ -10,12 +10,12 @@
 
 <div
   role="alert"
-  class="alert alert-soft alert-vertical sm:alert-horizontal w-full"
+  class="alert alert-soft alert-horizontal w-full"
 >
   <div class="py-3">
     <h3 class="mb-4 text-xl font-bold">{@html $_("firstTimeGuide.header")}</h3>
-    <p>{@html $_("firstTimeGuide.paragraphOpener")}</p>
-    <ul class="list-inside list-disc">
+    <p class="mb-2">{@html $_("firstTimeGuide.paragraphOpener")}</p>
+    <ul class="list-outside list-disc gap-y-3 pl-5">
       <li>{@html $_("firstTimeGuide.tip1")}</li>
       {#if !forProducerGen}
         <li>{@html $_("firstTimeGuide.tip2")}</li>
