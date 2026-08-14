8c474ee3f7ce1568f47ad1fcc9800314f31e83b7
ccxtwf <theweirdfandom@gmail.com>
2026-08-14 23:42:49 +0700

    FEAT: Allow simple copying & pasting of diacritics from the glossary

--- src/lib/components/reusables/DiacriticsGlossary.svelte
+++ src/lib/components/reusables/DiacriticsGlossary.svelte
@@ -1,7 +1,36 @@
 <script lang="ts">
+  import { _ } from "svelte-i18n";
   import ClosableCard from "./ClosableCard.svelte";
 
   let { onclose } = $props();
+
+  let tooltip: HTMLDivElement;
+
+  async function copyDiacritic(e: Event) {
+    try {
+      const el = e.currentTarget as HTMLDivElement;
+      const value = el.innerText;
+      await navigator.clipboard.writeText(value);
+      showSuccessfullyCopiedNotice(el);
+    } catch (err) {
+      console.error("Failed to copy text: ", err);
+    }
+  }
+
+  function showSuccessfullyCopiedNotice(el: HTMLDivElement) {
+    const bound = el.parentNode!.parentElement!;
+    const child = el.getBoundingClientRect();
+    const parent = bound.getBoundingClientRect();
+    const top = child.top - parent.top;
+    const left = child.left - parent.left + child.width / 2;
+
+    tooltip.style.cssText = `top:${top}px; left:${left}px;`;
+    tooltip.classList.remove("hidden");
+
+    setTimeout(() => {
+      tooltip.classList.add("hidden");
+    }, 1000);
+  }
 </script>
 
 <ClosableCard {onclose}>
@@ -19,17 +48,23 @@
     ["ō", "ó", "ǒ", "ò"],
     ["Ō", "Ó", "Ǒ", "Ò"],
   ]}
-  <div class="chars grid w-full grid-cols-2 gap-y-3 md:grid-cols-3 lg:grid-cols-4">
+  <div class="chars relative grid w-full grid-cols-2 gap-y-3 md:grid-cols-3 lg:grid-cols-4">
     {#each chars as charGroup}
       <div class="flex flex-row gap-x-2">
         {#each charGroup as char}
           <div
-            class="bg-neutral text-neutral-content inline-block w-8 rounded px-1 py-1.5 text-center"
+            class="bg-neutral text-neutral-content inline-block w-8 cursor-pointer rounded px-1 py-1.5 text-center"
+            onclick={copyDiacritic}
           >
             {char}
           </div>
         {/each}
       </div>
     {/each}
+    <div
+      class="tooltip tooltip-open absolute hidden"
+      data-tip={$_("formActions.copiedOutput")}
+      bind:this={tooltip}
+    ></div>
   </div>
 </ClosableCard>
