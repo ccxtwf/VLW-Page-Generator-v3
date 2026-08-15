58229e20b35562560d8d4acb489f6a56757851c9
ccxtwf <theweirdfandom@gmail.com>
2026-08-15 17:55:17 +0700

    IMPROVE: Keep tooltip open on hover

--- src/lib/components/reusables/Tooltip.svelte
+++ src/lib/components/reusables/Tooltip.svelte
@@ -1,35 +1,35 @@
 <script lang="ts">
+  import { _ } from "svelte-i18n";
   import type { Snippet } from "svelte";
 
   interface ToolTipProps {
     required?: boolean;
-    tooltipClasses?: string;
-    simpleTooltip?: string;
     children?: Snippet;
   }
 
-  let {
-    tooltipClasses = "tooltip-top tooltip-end sm:tooltip-center",
-    simpleTooltip,
-    required = false,
-    children,
-  }: ToolTipProps = $props();
+  let { required = false, children }: ToolTipProps = $props();
 </script>
 
-<div
-  class={["tooltip", tooltipClasses]}
-  data-tip={simpleTooltip}
->
+<div role="tooltip" class="dropdown dropdown-hover relative">
   {#if children}
-    <div class="tooltip-content">
+    <div
+      class="dropdown-content bg-neutral w-60 text-xs rounded-sm p-2 absolute max-sm:right-0 sm:left-1/2 bottom-0 sm:-translate-x-1/2 translate-y-full"
+    >
       {@render children()}
+      {#if required}
+        <div class="mt-2">*<em>{$_("required")}</em></div>
+      {/if}
     </div>
   {/if}
   <div
     class="flex h-4 w-4 cursor-pointer items-center justify-center rounded-full border-1 border-gray-400 text-sm font-bold text-gray-500 transition-colors select-none hover:bg-gray-100"
   >
     {#if required}
-      <div class="absolute -top-px right-0 translate-x-1/2 -translate-y-1/4 text-red-500">*</div>
+      <div
+        class="absolute -top-px right-0 translate-x-1/2 -translate-y-1/4 text-red-500"
+      >
+        *
+      </div>
     {/if}
     ?
   </div>
