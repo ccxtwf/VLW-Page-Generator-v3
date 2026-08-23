353deeb1b3c163bbe148cfdc081b8dd23cce4d2d
ccxtwf <theweirdfandom@gmail.com>
2026-08-23 12:16:31 +0700

    FEAT: Setup Vitest component tests

---
+++ __tests__/components/browsersetup.ts
@@ -0,0 +1,8 @@
+import "./declarations.d";
+
+import "./style.css";
+import "../../src/i18n";
+import "../../src/lib/components/handsontables/registration";
+
+import { locale } from "svelte-i18n";
+locale.set("en"); // oxlint-disable-line no-floating-promises
