e7bbee995b6e1620837343399f1fa75ba670cfc6
ccxtwf <theweirdfandom@gmail.com>
2026-09-20 19:32:31 +0700

    tests

--- __tests__/browsertests/themeToggle.spec.ts
+++ __tests__/browsertests/themeToggle.spec.ts
@@ -17,21 +17,24 @@ function getThemeToggle(page: Page): Locator {
   return themeToggle;
 }
 
-const rxThemeToggledCssClass = /(^|\s)theme-toggled(\s|$)/;
-
-function evaluateExpectedThemeToggledInThemeNavbarDropdown(expectedTheme: string) {
-  return (
-    document.querySelector(`input[type="radio"][value="${expectedTheme}"]`)! as HTMLInputElement
-  ).checked;
+function getLyricsTable(page: Page): Locator {
+  const lyricsTable = page.locator("#lyrics .ht-root-wrapper");
+  return lyricsTable;
 }
 
-async function setThemeChangedEventListener(page: Page) {
+function computeColorScheme() {
+  return window.getComputedStyle(document.body).getPropertyValue("color-scheme");
+}
+
+const rxThemeToggledCssClass = /(^|\s)theme-toggled(\s|$)/;
+
+async function spyOnEventListener(page: Page, event: "themeChanged" | "lyricsThemeToggled") {
   const eventListener = vi.fn();
   await page.exposeFunction("spyEventListener", eventListener);
-  await page.evaluate(() => {
+  await page.evaluate((event) => {
     //@ts-ignore
-    window.addEventListener("themeChanged", window.spyEventListener);
-  });
+    window.addEventListener(event, window.spyEventListener);
+  }, event);
   return eventListener;
 }
 
@@ -45,9 +48,7 @@ test.describe("Theming - Initial Load State", () => {
   test("Page should be set to automatic light mode", async ({ page }, {
     project: { name: browser },
   }) => {
-    let colorScheme = await page.evaluate(() => {
-      return window.getComputedStyle(document.documentElement).getPropertyValue("color-scheme");
-    });
+    let colorScheme = await page.evaluate(computeColorScheme);
     expect(colorScheme).toEqual("light");
 
     await page.screenshot({
@@ -61,15 +62,185 @@ test.describe("Theming - Initial Load State", () => {
   }) => {
     // prefer dark mode
     await page.emulateMedia({ colorScheme: "dark" });
-    let colorScheme = await page.evaluate(() => {
-      return window.getComputedStyle(document.documentElement).getPropertyValue("color-scheme");
-    });
+    let colorScheme = await page.evaluate(computeColorScheme);
     expect(colorScheme).toEqual("dark");
 
     await page.screenshot({
       path: `${getSnapshotsDir(browser)}/doc/auto-dark-mode.png`,
       fullPage: true,
     });
+  });
+
+  test('Page should remember its selected theme (select theme "Dark" on an OS that prefers Light Mode)', async ({
+    page,
+  }) => {
+    // prefer light mode
+    await page.emulateMedia({ colorScheme: "light" });
+    let colorScheme = await page.evaluate(computeColorScheme);
+    expect(colorScheme).toEqual("light");
+
+    expect(await page.localStorage.getItem("preferred-light-theme")).toBeNull();
+    expect(await page.localStorage.getItem("preferred-dark-theme")).toBeNull();
+    expect(await page.localStorage.getItem("preferred-theme")).toBeNull();
+
+    const themeToggleDropdown = getThemeNavbarDropdown(page);
+    await expect(themeToggleDropdown).toBeVisible();
+
+    await themeToggleDropdown.click();
+    await page.getByRole("radio", { name: "Dark" }).click();
+    await page.waitForFunction(
+      async () => {
+        const colorScheme = window
+          .getComputedStyle(document.documentElement)
+          .getPropertyValue("color-scheme");
+        return colorScheme === "dark";
+      },
+      null,
+      { timeout: 1000 },
+    );
+
+    expect(await page.localStorage.getItem("preferred-light-theme")).toBeNull();
+    expect(await page.localStorage.getItem("preferred-dark-theme")).toBe("dark");
+    expect(await page.localStorage.getItem("preferred-theme")).toBe("dark");
+
+    await page.reload();
+
+    const themeIsSetInBody = await page.evaluate(() => {
+      return window.document.body.getAttribute("data-theme") === "dark";
+    });
+    expect(themeIsSetInBody).toBe(true);
+
+    colorScheme = await page.evaluate(computeColorScheme);
+    expect(colorScheme).toEqual("dark");
+  });
+
+  test('Page should remember its selected theme (select theme "Nord" on an OS that prefers Light Mode)', async ({
+    page,
+  }) => {
+    // prefer light mode
+    await page.emulateMedia({ colorScheme: "light" });
+    let colorScheme = await page.evaluate(computeColorScheme);
+    expect(colorScheme).toEqual("light");
+
+    expect(await page.localStorage.getItem("preferred-light-theme")).toBeNull();
+    expect(await page.localStorage.getItem("preferred-dark-theme")).toBeNull();
+    expect(await page.localStorage.getItem("preferred-theme")).toBeNull();
+
+    const themeToggleDropdown = getThemeNavbarDropdown(page);
+    await expect(themeToggleDropdown).toBeVisible();
+
+    await themeToggleDropdown.click();
+    await page.getByRole("radio", { name: "Nord" }).click();
+    await page.waitForFunction(
+      async () => {
+        const colorScheme = window
+          .getComputedStyle(document.documentElement)
+          .getPropertyValue("color-scheme");
+        return colorScheme === "light";
+      },
+      null,
+      { timeout: 1000 },
+    );
+
+    expect(await page.localStorage.getItem("preferred-light-theme")).toBe("nord");
+    expect(await page.localStorage.getItem("preferred-dark-theme")).toBeNull();
+    expect(await page.localStorage.getItem("preferred-theme")).toBe("nord");
+
+    await page.reload();
+
+    const themeIsSetInBody = await page.evaluate(() => {
+      return window.document.body.getAttribute("data-theme") === "nord";
+    });
+    expect(themeIsSetInBody).toBe(true);
+
+    colorScheme = await page.evaluate(computeColorScheme);
+    expect(colorScheme).toEqual("light");
+  });
+
+  test('Page should remember its selected theme (select theme "Light" on an OS that prefers Dark Mode)', async ({
+    page,
+  }) => {
+    // prefer light mode
+    await page.emulateMedia({ colorScheme: "dark" });
+    let colorScheme = await page.evaluate(computeColorScheme);
+    expect(colorScheme).toEqual("dark");
+
+    expect(await page.localStorage.getItem("preferred-light-theme")).toBeNull();
+    expect(await page.localStorage.getItem("preferred-dark-theme")).toBeNull();
+    expect(await page.localStorage.getItem("preferred-theme")).toBeNull();
+
+    const themeToggleDropdown = getThemeNavbarDropdown(page);
+    await expect(themeToggleDropdown).toBeVisible();
+
+    await themeToggleDropdown.click();
+    await page.getByRole("radio", { name: "Light" }).click();
+    await page.waitForFunction(
+      async () => {
+        const colorScheme = window
+          .getComputedStyle(document.documentElement)
+          .getPropertyValue("color-scheme");
+        return colorScheme === "light";
+      },
+      null,
+      { timeout: 1000 },
+    );
+
+    expect(await page.localStorage.getItem("preferred-light-theme")).toBe("corporate");
+    expect(await page.localStorage.getItem("preferred-dark-theme")).toBeNull();
+    expect(await page.localStorage.getItem("preferred-theme")).toBe("corporate");
+
+    await page.reload();
+
+    const themeIsSetInBody = await page.evaluate(() => {
+      return window.document.body.getAttribute("data-theme") === "corporate";
+    });
+    expect(themeIsSetInBody).toBe(true);
+
+    colorScheme = await page.evaluate(computeColorScheme);
+    expect(colorScheme).toEqual("light");
+  });
+
+  test('Page should remember its selected theme (select theme "Gato" on an OS that prefers Dark Mode)', async ({
+    page,
+  }) => {
+    // prefer light mode
+    await page.emulateMedia({ colorScheme: "dark" });
+    let colorScheme = await page.evaluate(computeColorScheme);
+    expect(colorScheme).toEqual("dark");
+
+    expect(await page.localStorage.getItem("preferred-light-theme")).toBeNull();
+    expect(await page.localStorage.getItem("preferred-dark-theme")).toBeNull();
+    expect(await page.localStorage.getItem("preferred-theme")).toBeNull();
+
+    const themeToggleDropdown = getThemeNavbarDropdown(page);
+    await expect(themeToggleDropdown).toBeVisible();
+
+    await themeToggleDropdown.click();
+    await page.getByRole("radio", { name: "Gato" }).click();
+    await page.waitForFunction(
+      async () => {
+        const colorScheme = window
+          .getComputedStyle(document.documentElement)
+          .getPropertyValue("color-scheme");
+        return colorScheme === "dark";
+      },
+      null,
+      { timeout: 1000 },
+    );
+
+    expect(await page.localStorage.getItem("preferred-light-theme")).toBeNull();
+    expect(await page.localStorage.getItem("preferred-dark-theme")).toBe("gato");
+    expect(await page.localStorage.getItem("preferred-theme")).toBe("gato");
+
+    await page.reload();
+
+    const themeIsSetInBody = await page.evaluate(() => {
+      return window.document.body.getAttribute("data-theme") === "gato";
+    });
+    expect(themeIsSetInBody).toBe(true);
+
+    colorScheme = await page.evaluate(computeColorScheme);
+    expect(colorScheme).toEqual("dark");
   });
 });
 
@@ -86,7 +257,7 @@ test.describe("ThemeNavbarDropdown", () => {
     const themeToggleDropdown = getThemeNavbarDropdown(page);
     await expect(themeToggleDropdown).toBeVisible();
 
-    const spy = await setThemeChangedEventListener(page);
+    const spy = await spyOnEventListener(page, "themeChanged");
 
     await themeToggleDropdown.click();
     await page.getByRole("radio", { name: "Dark" }).click();
@@ -133,7 +304,7 @@ test.describe("ThemeNavbarDropdown", () => {
     const themeToggleDropdown = getThemeNavbarDropdown(page);
     await expect(themeToggleDropdown).toBeVisible();
 
-    const spy = await setThemeChangedEventListener(page);
+    const spy = await spyOnEventListener(page, "themeChanged");
 
     await themeToggleDropdown.click();
     await page.getByRole("radio", { name: "Light" }).click();
@@ -196,7 +367,7 @@ test.describe("ThemeToggle (song page)", () => {
     await expect(themeToggleContainer).toHaveClass(rxThemeToggledCssClass);
   });
 
-  test("Theme toggle should apply `color-scheme` (@media prefers: light mode)", async ({ page }, {
+  test("Theme toggle should toggle dark mode on the lyrics table", async ({ page }, {
     project: { name: browser },
   }) => {
     const themeToggle = getThemeToggle(page);
@@ -206,23 +377,15 @@ test.describe("ThemeToggle (song page)", () => {
     const themeToggleContainer = getThemeToggleContainer(page);
     await expect(themeToggleContainer).not.toHaveClass(rxThemeToggledCssClass);
 
-    const spy = await setThemeChangedEventListener(page);
+    const lyricsTable = getLyricsTable(page);
+    await expect(lyricsTable).toBeVisible();
+
+    const spy = await spyOnEventListener(page, "lyricsThemeToggled");
 
     await themeToggle.click();
     viExpect(spy).toHaveBeenCalled();
     await expect(themeToggleContainer).toHaveClass(rxThemeToggledCssClass);
-    await page.waitForFunction(
-      async () => {
-        const colorScheme = window
-          .getComputedStyle(document.documentElement)
-          .getPropertyValue("color-scheme");
-        return colorScheme === "dark";
-      },
-      null,
-      { timeout: 1000 },
-    );
-    /* Expect dark mode to be toggled on the navbar's theme toggle */
-    await page.evaluate(evaluateExpectedThemeToggledInThemeNavbarDropdown, "dark");
+    await expect(lyricsTable).toHaveClass(/ht-theme-dark/);
     /* Screenshot */
     await page.screenshot({
       path: `${getSnapshotsDir(browser)}/doc/song-page-lyrics-toggle-dark-mode.png`,
@@ -232,25 +395,14 @@ test.describe("ThemeToggle (song page)", () => {
     await themeToggle.click();
     viExpect(spy).toHaveBeenCalled();
     await expect(themeToggleContainer).not.toHaveClass(rxThemeToggledCssClass);
-    await page.waitForFunction(
-      async () => {
-        const colorScheme = window
-          .getComputedStyle(document.documentElement)
-          .getPropertyValue("color-scheme");
-        return colorScheme === "light";
-      },
-      null,
-      { timeout: 1000 },
-    );
-    /* Expect light mode to be toggled on the navbar's theme toggle */
-    await page.evaluate(evaluateExpectedThemeToggledInThemeNavbarDropdown, "corporate");
+    await expect(lyricsTable).toHaveClass(/ht-theme-light/);
     await page.screenshot({
       path: `${getSnapshotsDir(browser)}/doc/song-page-lyrics-toggle-light-mode.png`,
       fullPage: true,
     });
   });
 
-  test("Theme toggle should apply `color-scheme` (@media prefers: dark mode)", async ({ page }) => {
+  test("Theme toggle should toggle light mode on the lyrics table", async ({ page }) => {
     await page.emulateMedia({ colorScheme: "dark" });
     await page.reload();
 
@@ -264,39 +416,20 @@ test.describe("ThemeToggle (song page)", () => {
     const themeToggleContainer = getThemeToggleContainer(page);
     await expect(themeToggleContainer).toHaveClass(rxThemeToggledCssClass);
 
-    const spy = await setThemeChangedEventListener(page);
+    const lyricsTable = getLyricsTable(page);
+    await expect(lyricsTable).toBeVisible();
+
+    const spy = await spyOnEventListener(page, "lyricsThemeToggled");
 
     await themeToggle.click();
     viExpect(spy).toHaveBeenCalled();
     await expect(themeToggleContainer).not.toHaveClass(rxThemeToggledCssClass);
-    await page.waitForFunction(
-      async () => {
-        const colorScheme = window
-          .getComputedStyle(document.documentElement)
-          .getPropertyValue("color-scheme");
-        return colorScheme === "light";
-      },
-      null,
-      { timeout: 1000 },
-    );
-    /* Expect light mode to be toggled on the navbar's theme toggle */
-    await page.evaluate(evaluateExpectedThemeToggledInThemeNavbarDropdown, "corporate");
+    await expect(lyricsTable).toHaveClass(/ht-theme-light/);
 
     await themeToggle.click();
     viExpect(spy).toHaveBeenCalled();
     await expect(themeToggleContainer).toHaveClass(rxThemeToggledCssClass);
-    await page.waitForFunction(
-      async () => {
-        const colorScheme = window
-          .getComputedStyle(document.documentElement)
-          .getPropertyValue("color-scheme");
-        return colorScheme === "dark";
-      },
-      null,
-      { timeout: 1000 },
-    );
-    /* Expect dark mode to be toggled on the navbar's theme toggle */
-    await page.evaluate(evaluateExpectedThemeToggledInThemeNavbarDropdown, "dark");
+    await expect(lyricsTable).toHaveClass(/ht-theme-dark/);
   });
 });
 
@@ -332,7 +465,7 @@ test.describe("ThemeToggle (lyrics editor page)", () => {
     await expect(themeToggleContainer).toHaveClass(rxThemeToggledCssClass);
   });
 
-  test("Theme toggle should apply `color-scheme` (@media prefers: light mode)", async ({ page }, {
+  test("Theme toggle should toggle dark mode on the lyrics table", async ({ page }, {
     project: { name: browser },
   }) => {
     const themeToggle = getThemeToggle(page);
@@ -342,23 +475,15 @@ test.describe("ThemeToggle (lyrics editor page)", () => {
     const themeToggleContainer = getThemeToggleContainer(page);
     await expect(themeToggleContainer).not.toHaveClass(rxThemeToggledCssClass);
 
-    const spy = await setThemeChangedEventListener(page);
+    const lyricsTable = getLyricsTable(page);
+    await expect(lyricsTable).toBeVisible();
+
+    const spy = await spyOnEventListener(page, "lyricsThemeToggled");
 
     await themeToggle.click();
     viExpect(spy).toHaveBeenCalled();
     await expect(themeToggleContainer).toHaveClass(rxThemeToggledCssClass);
-    await page.waitForFunction(
-      async () => {
-        const colorScheme = window
-          .getComputedStyle(document.documentElement)
-          .getPropertyValue("color-scheme");
-        return colorScheme === "dark";
-      },
-      null,
-      { timeout: 1000 },
-    );
-    /* Expect dark mode to be toggled on the navbar's theme toggle */
-    await page.evaluate(evaluateExpectedThemeToggledInThemeNavbarDropdown, "dark");
+    await expect(lyricsTable).toHaveClass(/ht-theme-dark/);
     /* Screenshot */
     await page.screenshot({
       path: `${getSnapshotsDir(browser)}/doc/lyrics-editor-lyrics-toggle-dark-mode.png`,
@@ -368,25 +493,14 @@ test.describe("ThemeToggle (lyrics editor page)", () => {
     await themeToggle.click();
     viExpect(spy).toHaveBeenCalled();
     await expect(themeToggleContainer).not.toHaveClass(rxThemeToggledCssClass);
-    await page.waitForFunction(
-      async () => {
-        const colorScheme = window
-          .getComputedStyle(document.documentElement)
-          .getPropertyValue("color-scheme");
-        return colorScheme === "light";
-      },
-      null,
-      { timeout: 1000 },
-    );
-    /* Expect light mode to be toggled on the navbar's theme toggle */
-    await page.evaluate(evaluateExpectedThemeToggledInThemeNavbarDropdown, "corporate");
+    await expect(lyricsTable).toHaveClass(/ht-theme-light/);
     await page.screenshot({
       path: `${getSnapshotsDir(browser)}/doc/lyrics-editor-lyrics-toggle-light-mode.png`,
       fullPage: true,
     });
   });
 
-  test("Theme toggle should apply `color-scheme` (@media prefers: dark mode)", async ({ page }) => {
+  test("Theme toggle should toggle light mode on the lyrics table", async ({ page }) => {
     await page.emulateMedia({ colorScheme: "dark" });
     await page.reload();
 
@@ -400,38 +514,19 @@ test.describe("ThemeToggle (lyrics editor page)", () => {
     const themeToggleContainer = getThemeToggleContainer(page);
     await expect(themeToggleContainer).toHaveClass(rxThemeToggledCssClass);
 
-    const spy = await setThemeChangedEventListener(page);
+    const lyricsTable = getLyricsTable(page);
+    await expect(lyricsTable).toBeVisible();
+
+    const spy = await spyOnEventListener(page, "lyricsThemeToggled");
 
     await themeToggle.click();
     viExpect(spy).toHaveBeenCalled();
     await expect(themeToggleContainer).not.toHaveClass(rxThemeToggledCssClass);
-    await page.waitForFunction(
-      async () => {
-        const colorScheme = window
-          .getComputedStyle(document.documentElement)
-          .getPropertyValue("color-scheme");
-        return colorScheme === "light";
-      },
-      null,
-      { timeout: 1000 },
-    );
-    /* Expect light mode to be toggled on the navbar's theme toggle */
-    await page.evaluate(evaluateExpectedThemeToggledInThemeNavbarDropdown, "corporate");
+    await expect(lyricsTable).toHaveClass(/ht-theme-light/);
 
     await themeToggle.click();
     viExpect(spy).toHaveBeenCalled();
     await expect(themeToggleContainer).toHaveClass(rxThemeToggledCssClass);
-    await page.waitForFunction(
-      async () => {
-        const colorScheme = window
-          .getComputedStyle(document.documentElement)
-          .getPropertyValue("color-scheme");
-        return colorScheme === "dark";
-      },
-      null,
-      { timeout: 1000 },
-    );
-    /* Expect dark mode to be toggled on the navbar's theme toggle */
-    await page.evaluate(evaluateExpectedThemeToggledInThemeNavbarDropdown, "dark");
+    await expect(lyricsTable).toHaveClass(/ht-theme-dark/);
   });
 });
