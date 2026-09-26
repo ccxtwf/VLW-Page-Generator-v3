f3caff76c13853f24f4d1e907a992e6e645d134f
ccxtwf <theweirdfandom@gmail.com>
2026-09-26 20:07:24 +0700

    patch: Default number of rows in lyrics tables

--- src/lib/components/handsontables/LyricsFreeEditTable.svelte
+++ src/lib/components/handsontables/LyricsFreeEditTable.svelte
@@ -5,6 +5,7 @@
   import { getTheme } from "handsontable/themes";
   import { getLyricsEditContextMenu } from "./contextMenus/lyricsEdit";
 
+  import Song from "../../models/Song.svelte.js";
   import LyricRow from "../../models/children/LyricsRow.svelte";
   import type { ILyricsRow } from "../../models/schema.js";
   import { HANDSONTABLE_LICENSE_KEY } from "../../../config";
@@ -13,10 +14,10 @@
     determineColumnHeaders,
     removeColumnsAtIndexFromToggle,
   } from "../../utils/lyricsUtils";
+  import { isDarkModeActive } from "../../utils/themeUtils.js";
   import { registerUndoRedoActionOnDataLoad } from "./utils";
   import type { LyricsThemeToggledEventPayload } from "../../../schemas/events";
   import { LyricsTableHeader } from "../../../constants/tableHeaders";
-  import { isDarkModeActive } from "#src/lib/utils/themeUtils.js";
 
   interface LyricsTableFreeEditProps {
     id: string;
@@ -69,7 +70,7 @@
 
   export function resetState(): void {
     hot!.loadData(
-      Array(20)
+      Array(Song.defaultStartingRows)
         .fill(null)
         .map(() => Array(4).fill("")),
     );
